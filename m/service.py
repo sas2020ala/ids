96dd3117cb3c6cdf224d978a864dd1d98f6faa2d
nursultan <ids@decoritm.com>
2022-03-02 21:28:36 +0600

    added terminal

--- m/service.py
+++ m/service.py
@@ -30,6 +30,21 @@ dc = Database()
 dc.connect_to_cache()
 dc.connect()
 
+SEL_DEV = """
+    SELECT u.u_tel, ud.id
+    FROM device d 
+    INNER JOIN user_device ud on d.d_id = ud.d_id
+    INNER JOIN users u on ud.u_id = u.u_id
+    WHERE d.h_code = %s;
+    """
+
+
+up_dev = """
+    UPDATE user_device
+    SET r_status = 1
+    WHERE id = %s
+    """
+
 
 @app.route("/app/verify", methods=["GET", "POST"])
 def verify_app():
@@ -151,6 +166,163 @@ def user_register():
         Logger.error(f"m.service.verify_code: {sys.exc_info()[-1].tb_lineno}: {e}")
         return proto.msg_error("At the moment you cannot log in."
                                " Please contact your system administrator")
+
+
+@app.route("/checkDevice", methods=["POST"])
+def check_device():
+
+    try:
+        if not request.method == "POST":
+            return proto.msg_wrong("Wrong reuest")
+
+        data = json.loads(request.data)
+        con = dc.check()
+        cur = con.cursor()
+        cur.execute(SEL_DEV, (data['terminal'],))
+        res = cur.fetchone()
+        print(res)
+        if res and res[0] == data["email"]:
+
+            app_token = request.headers.get("access-token")
+
+            if not app_token:
+                return proto.msg_wrong("No access token")
+
+            e_mail = data["email"]
+
+            if e_mail and System.valid_email(e_mail):
+                app_i: int = dc.get_from_cache(app_token)["app_i"]
+
+                if user.is_registered(dc, e_mail, app_i, app_token) == 1:
+                    dc.save_to_cache(e_mail+"term", data["terminal"])
+                    return proto.msg_success({"msg": "Success"})
+                else:
+                    return proto.msg_error("At the moment you cannot log in."
+                                           " Please contact your system administrator")
+            else:
+                proto.msg_error("Wrong user e-mail")
+
+    except Exception as e:
+        Logger.error(f"m.service.verify_code: {sys.exc_info()[-1].tb_lineno}: {e}")
+        return proto.msg_error("At the moment you cannot log in."
+                               " Please contact your system administrator")
+
+
+@app.route("/saveDevice", methods=["POST"])
+def save_device():
+    tag = "SaveDevice"
+    cur = None
+    conn = None
+    try:
+        dat = json.loads(request.data)
+
+        conn = dc.check()
+        cur = conn.cursor()
+        # rd = dc.get_from_cache(f"{dat['email']}-{dat['code']}")
+        terminal_id = dc.get_from_cache(dat["email"]+"term")
+
+        if terminal_id:
+            cur.execute(up_dev, (terminal_id,))
+            return proto.msg_success({"own": dat["phone"], "tid": terminal_id})
+        app.logger.warning(f'{tag} - {dat} мәліметтер дұрыс емес')
+        return proto.msg_wrong("Wrong data")
+
+    except Exception as e:
+        Logger.error(f"m.service.verify_code: {sys.exc_info()[-1].tb_lineno}: {e}")
+        return proto.msg_error("At the moment you cannot log in."
+                               " Please contact your system administrator")
+
+
+# @app.route(f"{base_url}/checkDevice", methods=["POST"])
+# @access_token_required
+# def check_device_info():
+#     """Check Device Info (Pos terminal 1)
+#     Pos терминалда активатция жасау.
+#     Терминалдың иесі номерін және терминалдың id - ін жібереді.
+#     Егер мәліметтер дұрыс болса қолданушыны растау мақсатында номеріне
+#     смс код жіберіледі.
+#     :return:
+#     """
+#     tag = "CheckDevice"
+#     cur = None
+#     conn = None
+#     try:
+#         dat = json.loads(request.data)
+#
+#         conn = db.get_con()
+#         cur = conn.cursor()
+#         cur.execute(sql["sel_dev"], (dat["terminal"],))
+#         res = cur.fetchone()
+#         print(res)
+#         print(dat)
+#         if res and res[0] == dat["phone"]:
+#             tkn = get_access_token(request)
+#             appl_id = redis_db.read(tkn)["appl_id"]
+#
+#             result, _code = smss.send_sms(dat["phone"], appl_id)
+#             if "error" in result:
+#                 return proto.msg_error(result["error"])
+#
+#             redis_db.write(f"{dat['phone']}-{_code}", {"tid": res[1]})
+#             return proto.msg_success({"code": _code, "msg": "Success"})
+#         app.logger.warning(f'{tag} - {dat} мәліметтер дұрыс емес')
+#         return proto.msg_wrong("Wrong data")
+#     except Exception as e:
+#         app.logger.exception(e)
+#         return proto.msg_error("Error")
+#     finally:
+#         if cur:
+#             cur.close()
+#             db.close(conn)
+#
+#
+# @app.route(f"{base_url}/saveDevice", methods=["POST"])
+# @access_token_required
+# def save_device_info():
+#     """Save Device Info (Pos terminal 2)
+#     Pos терминалда активатция жасау.
+#     Қолданушы номеріне келген смс код дұрыс
+#     болса қосымша әрі қарай жұмыс жасау үшін
+#     терминалдың мәліметтері (status) өзгертіледі.
+#     Parameters
+#     ----------
+#     terminal_id: str
+#
+#     Returns
+#     -------
+#     {
+#         "sts": "s",
+#         "dat": {
+#             "own": "user phone",
+#             "tid": "terminal id"
+#         }
+#     }
+#     """
+#     tag = "SaveDevice"
+#     cur = None
+#     conn = None
+#     try:
+#         dat = json.loads(request.data)
+#
+#         conn = db.get_con()
+#         cur = conn.cursor()
+#         rd = redis_db.read(f"{dat['phone']}-{dat['code']}")
+#         terminal_id = rd["tid"]
+#
+#         if terminal_id:
+#             cur.execute(sql['up_dev'], (terminal_id,))
+#             return proto.msg_success({"own": dat["phone"], "tid": rd["tid"]})
+#         app.logger.warning(f'{tag} - {data} мәліметтер дұрыс емес')
+#         return proto.msg_wrong("Wrong data")
+#     except Exception as e:
+#         app.logger.exception(e)
+#         conn.rollback()
+#         return proto.msg_error("Error")
+#     finally:
+#         if cur:
+#             cur.close()
+#             db.close(conn)
+
 
 if __name__ == "__main__":
     if not os.path.exists(md.path):
